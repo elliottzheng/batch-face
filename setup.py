9d0917d442a2840de7f7969f60f71b9976d08064
Elliott Zheng <admin@hypercube.top>
2023-05-06 19:04:07 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ def read_requirements(filename):
 
 setup(
     name="batch-face",
-    version="1.2.0",
+    version="1.4.0",
     description="Batch Face Preprocessing for Modern Research",
     author="Elliott Zheng",
     author_email="admin@hypercube.top",
